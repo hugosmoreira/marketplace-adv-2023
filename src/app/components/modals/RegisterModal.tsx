6c936344e29335c32ad7de097fa769f771969027
Hugo Moreira <hugo.n4@gmail.com>
2023-05-27 23:17:26 -0700

    register modal working

--- src/app/components/modals/RegisterModal.tsx
+++ src/app/components/modals/RegisterModal.tsx
@@ -2,6 +2,7 @@
 
 import axios from 'axios';
 import { AiFillGithub } from "react-icons/ai";
+import { signIn } from "next-auth/react";
 import useLoginModal from "@/app/hooks/useLoginModal";
 import { FcGoogle } from "react-icons/fc";
 import { useCallback, useState } from "react";
@@ -9,6 +10,9 @@ import { toast } from "react-hot-toast";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import Modal from "./Modal";
+import Heading from '../Heading';
+import Input from '../inputs/Input';
+import Button from '../Button';
 
 
 const RegisterModal = () => {
@@ -51,6 +55,75 @@ const RegisterModal = () => {
         loginModal.onOpen();
       }, [registerModal, loginModal])
 
+      const bodyContent = (
+        <div className='flex flex-col gap-4'>
+          <Heading title="Welcome to Adrena" subtitle="Create an account!"/>
+          <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        label="Password"
+        type="password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+        </div>
+    )
+
+    const footerContent = (
+      <div className="flex flex-col gap-4 mt-3">
+        <hr />
+        <Button 
+          outline 
+          label="Continue with Google"
+          icon={FcGoogle}
+          onClick={() => signIn('google')} 
+        />
+        <Button 
+          outline 
+          label="Continue with Github"
+          icon={AiFillGithub}
+          onClick={() => signIn('github')}
+        />
+        <div 
+          className="
+            text-neutral-500 
+            text-center 
+            mt-4 
+            font-light
+          "
+        >
+          <p>Already have an account?
+            <span 
+              onClick={onToggle} 
+              className="
+                text-neutral-800
+                cursor-pointer 
+                hover:underline
+              "
+              > Log in</span>
+          </p>
+        </div>
+      </div>
+    )
+  
+
       
   
 
@@ -62,6 +135,7 @@ const RegisterModal = () => {
     actionLabel="Continue"
     onClose={registerModal.onClose}
     onSubmit={handleSubmit(onSubmit)}
+    body={bodyContent}
     
   />
   )
